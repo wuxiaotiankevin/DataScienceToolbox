a4da87f155d160f754dd6607c6997d94122deda8
Kevin <wuxiaotiankevin@gmail.com>
2018-04-16 16:15:57 -0400

    cpu status

---
+++ hardware_usage/cpuStatus.py
@@ -0,0 +1,8 @@
+import os, psutil
+
+
+def cpuStats():
+    pid = os.getpid()
+    py = psutil.Process(pid)
+    memoryUse = py.memory_info()[0] / 2. ** 30
+    print('memory GB:', "%.3f" % memoryUse)

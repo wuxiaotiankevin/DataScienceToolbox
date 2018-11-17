475f28428e1f99bcd9d2447702a295e4a0f87d86
Kevin Wu <wuxiaotiankevin@gmail.com>
2018-11-17 18:15:53 -0500

    delete cpu status

--- hardware_usage/cpuStatus.py
+++
@@ -1,8 +0,0 @@
-import os, psutil
-
-
-def cpuStats():
-    pid = os.getpid()
-    py = psutil.Process(pid)
-    memoryUse = py.memory_info()[0] / 2. ** 30
-    print('memory GB:', "%.3f" % memoryUse)

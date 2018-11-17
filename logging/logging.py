5fb8d929c2145200bd66ea45249b12064ea5d415
Kevin Wu <wuxiaotiankevin@gmail.com>
2018-11-17 09:09:34 -0500

    logging

--- logging/logging.py
+++ logging/logging.py
@@ -3,7 +3,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # create a file handler
-handler = logging.FileHandler('./download_picture.log')
+handler = logging.FileHandler('./LOG_NAME.log')
 handler.setLevel(logging.INFO)
 
 # create a logging format
@@ -12,3 +12,11 @@ handler.setFormatter(formatter)
 
 # add the handlers to the logger
 logger.addHandler(handler)
+
+# usage
+logger.debug('')
+logger.info('')
+logger.error('')
+logger.warning('')
+logger.error('')
+logger.critical('')
